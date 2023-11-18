2ac03c6bafac4c298868e56b0cb275223608f175
Ilia Filimonov <9991571155i@gmail.com>
2023-11-19 00:00:17 +0700

    Добавит страницу Features

---
+++ src/pages/Features.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/* страница с преимуществами */
+const Features = () => {
+  return (
+    <section className="pt-20">
+      <div className="container">
+        <h3>Features page</h3>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
