0f4b19a217184d0d926d3c45cd10f01b0e043eed
Ilia Filimonov <9991571155i@gmail.com>
2023-11-19 00:00:02 +0700

    Поправит работу тега head в корне проекта

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,19 +3,21 @@ import React from "react";
 
 /* Домашнаяя страница */
 const Home = () => {
-  <>
-    <Head>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>App</title>
-      <link rel="icon" href="./favicon.ico" />
-    </Head>
-    <main>
-      <div className="container">
-        <h1>Домашняя страница .</h1>
-      </div>
-    </main>
-  </>;
+  return (
+    <>
+      <Head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>App</title>
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <main>
+        <div className="container">
+          <h1>Домашняя страница .</h1>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
